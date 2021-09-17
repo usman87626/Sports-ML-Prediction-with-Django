514f067fdcfbbec6efb2aac2d5e417eec3ff2207
usman87626 <usman.arif20188@gmail.com>
2021-09-17 19:54:13 +0500

    First Commit

---
+++ dashboard/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('name','age','height','sex','predictions')
+admin.site.register(models.Data,DataAdmin)
